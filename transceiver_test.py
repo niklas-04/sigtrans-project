5684628c2e7dc65de2d338f8789da51465b9cf5a
LordSylt <os_smed@hotmail.se>
2025-12-12 15:03:12 +0100

    actually fixed transceiver_test

--- transceiver_test.py
+++ transceiver_test.py
@@ -5,37 +5,54 @@ import numpy as np
 from scipy import signal
 import sounddevice as sd
 
-from parameters import Tb, dt, Wc
+from parameters import Tb, dt, Wc, Ac
 
 N = 7
 fs = 1/dt
 
+def string_to_bits(s):
+    bits = []
+    for char in s:
+        binary = format(ord(char), '08b')
+        bits.extend(int(b) for b in binary)
+    return bits
+
 
 def demodulation_test():
-    #Transmitted bits
-    bits = np.array([1, 0 , 0, 0, 1, 1, 1, 0, 1 ,0, 1, 1, 0, 1, 0, 1, 0])
-    xb = wcs.encode_baseband_signal(bits, Tb, fs=fs)
 
+    string = input("Input: \n")
+    bitsString = string_to_bits(string)
+
+
+    #Transmitted bits
+    xb = wcs.encode_baseband_signal(bitsString, Tb, fs=fs)
 
     tt = np.arange(0, xb.shape[0])*dt
-    yI = xb * 2*np.cos(Wc*tt)
-    yQ = xb * 2*np.sin(Wc*tt)
+
+    #Modulation
+    xt = np.zeros_like(xb)
+    for i in range(len(xt)):
+        xt[i] = xb[i] * Ac * np.sin(Wc * tt[i])
 
 
+    #Demodulation
+    yI = xt * 2*np.cos(Wc*tt)
+    yQ = xt * 2*np.sin(Wc*tt)
 
-    # Demodulation
+
+    #Filtering away artifact frequency at 2000 Hz
     #b_lp, a_lp = signal.cheby2(N, 31, 1100, btype = 'lowpass', fs=fs, output='ba')
-    b_lp, a_lp = signal.butter(N, 1100, btype = 'lowpass', fs=fs, output='ba')
+    b_lp, a_lp = signal.butter(N, 200, btype = 'lowpass', fs=fs, output='ba')
 
     yI = signal.lfilter(b_lp, a_lp, yI)
     yQ = signal.lfilter(b_lp, a_lp, yQ)
 
     yb = yI + 1J * yQ
 
-    print(bits)
     br = wcs.decode_baseband_signal(yb, Tb, fs)
     print(br)
     data_rx = wcs.decode_string(br)
     print(f'Received: {data_rx} (no of bits: {len(br)}).')
+    
 
 demodulation_test()
